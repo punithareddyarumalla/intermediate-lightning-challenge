df35b05a34484cffd6380d5ef7b3fccc6109f507
edoardo@fxdigital.uk <edoardo@fxdigital.uk>
2022-07-22 14:40:32 +0100

    feat: add routes file

---
+++ src/routes/routes.js
@@ -0,0 +1,12 @@
+import Game from "../pages/Game";
+
+export default {
+  root: "game",
+  routes: [
+    {
+      path: "game",
+      component: Game,
+      // widgets: ["Navbar"],
+    },
+  ],
+};

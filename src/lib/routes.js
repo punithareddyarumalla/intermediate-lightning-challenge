e842bf380af33f24b3222af2c1e1580eecbf9770
Zayd Algawi <zayd@fxdigital.uk>
2022-07-22 16:21:13 +0100

    create Routes file
    import all pages and set the routes

---
+++ src/lib/routes.js
@@ -0,0 +1,22 @@
+import MainMenu from "../pages/MainMenu"
+import Highscore from "../pages/Highscore"
+import Game from "../pages/Game"
+
+
+export default {
+  root: "mainMenu", // Sets the main menu as the first / default page to open
+  routes: [
+    {
+      path: "mainMenu", // path to access the page
+      component: MainMenu, // Page
+    },
+    {
+      path: "highScore", 
+      component: Highscore, 
+    },
+    {
+      path: "game", 
+      component: Game,
+    },
+  ],
+};
